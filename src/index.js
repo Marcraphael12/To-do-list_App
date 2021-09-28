029d4dc3c3deda6c7e8a4845b5757d58f7e831c0
Marc Raphael <marcraphael979@gmail.com>
2021-09-28 12:44:30 +0100

    Import modules to create the to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import _ from 'lodash';
 import './style.css';
 
+import refresher from './refresher.js';
+import form from './form-inputs.js';
+import taskContainer from './new-task.js';
+import clearButton from './clear-task.js';
+import Task from './task-adder';
+
+const ul = document.createElement('ul');
+
+
+const container = document.querySelector('.container');
+container.appendChild(refresher());
+container.appendChild(form());
+container.appendChild(ul);
+container.appendChild(clearButton());
+
+const body = document.querySelector('body');
+body.appendChild(container);
+
+const myTasks = [];
+myTasks.push(new Task('Eat', false, 0));
+myTasks.push(new Task('Sleep', false, 1));
+myTasks.push(new Task('Code', false, 2));
+myTasks.push(new Task('Repeat', false, 3));
+
+myTasks.forEach((task) => {
+  ul.appendChild(taskContainer(task));
+});
