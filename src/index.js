eb069a003e556740729dab58b810730b81a285b9
Marc Raphael <marcraphael979@gmail.com>
2021-09-30 17:38:01 +0100

    Import module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import _ from 'lodash';
 import './style.css';
+import statusCheck from './status.js';
+
+document.getElementById('showListItem').addEventListener('click', (e) => {
+  if (e.target.classList.contains('checkboX')) {
+    statusCheck(e);
+  }
+});
