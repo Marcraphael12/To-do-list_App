af0639a38f83b33be85cd05eee33e23d69fa1504
Marc Raphael <marcraphael979@gmail.com>
2021-09-30 17:47:08 +0100

    Restart from zero

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import statusCheck from './status.js';
-
-document.getElementById('showListItem').addEventListener('click', (e) => {
-  if (e.target.classList.contains('checkboX')) {
-    statusCheck(e);
-  }
-});
