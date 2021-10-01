aaa0562c48be60cb8b042d7fa6484952d9e14ccc
Marc Raphael <marcraphael979@gmail.com>
2021-10-01 13:27:45 +0100

    Create the drop function: Save the status to local storage

--- src/dragDrop.js
+++ src/dragDrop.js
@@ -19,3 +19,17 @@ export function dragEnter(event) {
 export function dragLeave() {
   targetItem = null;
 }
+
+export function drop(event) {
+  event.preventDefault();
+  targetItem = document.getElementById(event.target.id);
+  current.parentElement.insertBefore(current, targetItem);
+
+  const children = Array.from(current.parentElement.children);
+  const updatedList = children.map((el, index) => ({
+    index,
+    completed: el.checked,
+    description: el.children[1].value,
+  }));
+  localStorage.setItem('TodoList', JSON.stringify(updatedList));
+}
