aa1092d9618c1459d40d81e1fe77f95e1a14fb24
Marc Raphael <marcraphael979@gmail.com>
2021-10-01 20:42:41 +0100

    import add and remove module

--- src/index.js
+++ src/index.js
@@ -1,96 +1,10 @@
 import './style.css';
+ import newTask from './addAndRemove.js';
 
-// need drag and funct
-// drag
-// start
-// end
-// leave
-// drag enter
-
-import {
-  dragStart,
-  allowDrop,
-  dragEnd,
-  drop,
-  dragEnter,
-  dragLeave,
-} from './dragDrop.js';
-
-import updateStatus from './status.js';
-
-const list = document.getElementById('list');
-
-let todoList = [
-  {
-    description: 'Eat',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Sleep',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Code',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Repeat',
-    completed: false,
-    index: 3,
-  },
-];
-
-function renderList(arr) {
-  list.innerHTML = arr.map((item) => `<li class='flex-row todo di btm-bdr' draggable='true' id='${item.index}'>
-<input type='checkbox' class='checkbox' data-id='${item.index}'  ${item.completed ? 'checked' : ''}>
-<input type='text' value='${item.description}' data-index='${item.index}'draggable='false' class='todo-text ${item.completed ? 'completed' : ''}'>
-<i class='fas fa-ellipsis-v dots' data-id='${item.index}'></i>
-</li>`).join('');
-
-  list.addEventListener('dragenter', dragEnter);
-
-  document.querySelectorAll('.todo').forEach((t) => {
-    t.addEventListener('dragstart', dragStart);
-    t.addEventListener('dragend', dragEnd);
-    t.addEventListener('dragenter', dragEnter);
-    t.addEventListener('dragleave', dragLeave);
-    t.addEventListener('drop', drop);
-    t.addEventListener('dragover', allowDrop);
-  });
-
-  document.querySelectorAll('.todo-text').forEach((text) => {
-    text.addEventListener('focus', (event) => {
-      document.querySelectorAll('.todo').forEach((t) => {
-        t.style.backgroundColor = '#fff';
-      });
-      event.target.parentNode.style.backgroundColor = '#fea';
-    });
-    text.addEventListener('blur', () => {
-      document.querySelectorAll('.todo').forEach((t) => {
-        t.style.backgroundColor = '#fff';
-      });
-    });
-  });
-
-  const checkboxes = document.querySelectorAll('.checkbox');
-  checkboxes.forEach((chbox) => {
-    chbox.addEventListener('change', updateStatus);
-  });
+ document.getElementById('input-task').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    const v = e.target.value;
+    e.target.value = '';
+    e.preventDefault();
+    newTask(v);
 }
-
-window.addEventListener('DOMContentLoaded', () => {
-  // if todo list is !empty, get data from it and render
-  if (localStorage.getItem('TodoList')) {
-    todoList = JSON.parse(localStorage.getItem('TodoList'));
-  } else {
-    localStorage.setItem(
-      'TodoList',
-      JSON.stringify(todoList.sort((a, b) => +a.index - +b.index)),
-    );
-  }
-
-  renderList(todoList.sort((a, b) => +a.index - +b.index));
-});
