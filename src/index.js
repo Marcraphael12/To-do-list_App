4e2d237710f7f726a20236fe1a5b3d2c4c3b98bc
Marc Raphael <marcraphael979@gmail.com>
2021-10-01 13:33:09 +0100

    Import drag and drop module: with all its functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+
+// need drag and funct
+  //drag
+      //start
+      //end
+      //leave
+      //drag enter
+
+import {
+  dragStart,
+  allowDrop,
+  dragEnd,
+  drop,
+  dragEnter,
+  dragLeave,
+} from './dragDrop.js';
