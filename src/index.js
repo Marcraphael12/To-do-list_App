d839bd17a6efd891c14eb05170e4ab6894206571
Marc Raphael <marcraphael979@gmail.com>
2021-10-01 23:13:50 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import _ from 'lodash';
 import './style.css';
 import newTask from './AddandRemove.js';
 
- document.getElementById('input-task').addEventListener('keypress', (e) => {
+document.getElementById('input-task').addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     const v = e.target.value;
     e.target.value = '';
