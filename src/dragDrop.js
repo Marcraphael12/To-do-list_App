60fd7580235c92c1447487dd68cac4511c4325a4
Marc Raphael <marcraphael979@gmail.com>
2021-10-01 13:23:53 +0100

    Add drag start and drag end functions

--- src/dragDrop.js
+++ src/dragDrop.js
@@ -1,2 +1,12 @@
 let current = null;
 let targetItem = null;
+
+export function dragStart() {
+  current = this;
+  current.classList.add('current-active');
+}
+
+export function dragEnd() {
+  current.classList.remove('current-active');
+  current = null;
+}
